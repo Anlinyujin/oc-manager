bc9f1db503fa13e6649d38e1895ddaf99f018ca2
Anlinyujin <1769352140@qq.com>
2026-02-12 05:15:52 +0800

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'oc-manager-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './css/style.css',
+  './js/ui.js',
+  './js/data.js',
+  './js/markdown.js',
+  './js/characters.js',
+  './js/notes.js',
+  './js/tags.js',
+  './js/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
